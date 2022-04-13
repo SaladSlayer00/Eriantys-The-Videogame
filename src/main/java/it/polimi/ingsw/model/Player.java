354a293b75dcade1dc9ad3265fac84f9eff7b0e5
ThetaKimbi <101363816+ThetaKimbi@users.noreply.github.com>
2022-04-13 21:50:46 +0200

    First commit of the Maven project.

--- src/main/java/it/polimi/ingsw/model/Player.java
+++ src/main/java/it/polimi/ingsw/model/Player.java
@@ -1,52 +1,58 @@
 package it.polimi.ingsw.model;
 import it.polimi.ingsw.model.Deck;
 
+
+// Player class represents the user
 public class Player {
 
+    //attributes of the class Player
     private String name;
     private Deck deck;
     private State state;
     private Dashboard dashboard;
     private int coins;
 
+    //methods of the Player
+    //it returns the player's name
     public String getName() {
         return name;
     }
-
+    //it returns the player's deck
     public Deck getDeck() {
         return deck;
     }
-
+    //it returns the player's dashboard
     public Dashboard getDashboard() {
         return dashboard;
     }
-
+    //it returns the player's number of coins
     public int getCoins() {
         return coins;
     }
-
+    //it returns the player's state
     public State getState() {
         return state;
     }
 
 
-
+    //it draws a card from the assistant deck
     public Assistant drawCard() {
 
-    }
 
+    }
+    //it allows the player to choose the island
     public void chooseCloud( ) {
 
     }
-
+    //it adds a coin
     public void addCoin() {
 
     }
-
+    //it remove a coin
     public void removeCoin() {
 
     }
-
+    //it change the player's state
     public void changeState() {
 
     }
