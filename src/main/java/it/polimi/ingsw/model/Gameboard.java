51899f1e9907427c78a5d4bc9e5f1b7a1d484d99
mcrin <alice.maccarini@mail.polimi.it>
2022-04-13 16:34:08 +0200

    spero non stia facendo un mezzo macello

--- src/main/java/it/polimi/ingsw/model/Gameboard.java
+++ src/main/java/it/polimi/ingsw/model/Gameboard.java
@@ -10,15 +10,36 @@ public class Gameboard{
     private ArrayList<Professor> professors;
     private int motherNature;
 
-    //methods of the Gameboards
+    //methods of the Gameboard
     //it places Mother Nature on a random island
     public void placeMother(){
-
+        int random = (int)(Math.random() * 11);
+        islands.get(random).motherNature = true;
+        if(random < 6) {
+            //no students should be on the islands opposite at mn at the start of the match
+            islands.get(random + 6)...;
+        }
+        else{
+            //ditto
+            islands.get(random - 6)...;
+        }
     }
 
     //it merges two island together
-    public void mergeIslands(int one, int two){
-
+    public void mergeIslands(int one, int two) {
+        islands.get(one).dimension =+ islands.get(two).dimension;
+        if(islands.get(one).motherNature == false){
+            islands.get(one).addMother;
+        }
+        for(Color c : islands.get(two).students.keySet()){
+            for(Color k : islands.get(one).students.keySet()){
+                if(k.equals(c)){
+                    //how should this thing go on???
+                }
+            }
+        }
+        //should be found a smart way to merge the two map together ???
+        islands.remove(two);
     }
 
     //it calculates the influence of a player on an island
@@ -32,52 +53,69 @@ public class Gameboard{
     //class Island
     private class Island {
         //attributes of the class Island
-        private Map<Color, ArrayList<Student>> students;  ???
+        private Map<Color, ArrayList<Student>> students; // ???
         private boolean motherNature;
         private boolean hasTower;
-        private Team tower;  ???
+        private Team tower;  // ???
         private int dimension = 1;
         int index;
 
         //methods of the class
         //it adds a student to the island
-        public void addStudent(student Student) {
+        public void addStudent(Student student) {
 
         }
 
         //it adds a tower of a specific team to an island
-        public void addTower(team Team) {
-            //idea: if hasTower == true then we can throw an exception
-            //OR MAYBE we can just see if the person that asked for the tower
-            //has more influence than the other player has on the island
-            //else we add the tower and then hasTower = true
-            //and tower = team;
+        public void addTower(team Team) throws AlreadyATowerException{
+            if(hasTower == true){
+                thorw new AlreadyATowerException();
+            }
+            else{
+                hasTower = true;
+            }
         }
 
-        //it returns the color of the tower
-        public Team getColor() {
+        //exception to handle the moment when a tower is already on an island
+        //!!!
+        //colorToString(tower) may be a method to get the color of the tower we want as a string
+        //!!!
+        public class AlreadyATowerException extends Exception{
+            AlreadyATowerException(){
+                super("There is already a tower on this island. It's " + colorToString(getColor()) + "!");
+            }
+        }
+
+        //it returns the color of the tower TO CHECK!!!!!!
+        public Team getColor() throws NoTowerException{
             if (hasTower == true) {
                 return tower;
             } else {
-                //exception???
+                throw new NoTowerException();
+                getInfluence(player);
+            }
+        }
+
+        //exception to handle the moment when no tower is found on an island
+        public class NoTowerException extends Exception{
+            NoTowerException(){
+                super("There isn't any tower on this island");
             }
         }
 
         //it returns the influence
-        public int getInfluence(player Player){
+        public int getInfluence(Player player){
 
 
         }
 
         //it adds Mother Nature on the island
         public void addMother(){
-            //maybe we should add a way to check if this action is legit
             motherNature = true;
         }
 
         //it removes Mother Nature
         public void removeMother(){
-            //ditto for this method
             motherNature = false;
         }
 
