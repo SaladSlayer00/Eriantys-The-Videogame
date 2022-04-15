9f73b083515981329b0e2c3353b8ca50d48c1f26
ThetaKimbi <101363816+ThetaKimbi@users.noreply.github.com>
2022-04-16 01:56:26 +0200

    changes : Dashboard,player

--- src/main/java/it/polimi/ingsw/model/Player.java
+++ src/main/java/it/polimi/ingsw/model/Player.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.model;
 import it.polimi.ingsw.model.Deck;
 import it.polimi.ingsw.model.Color;
 import java.util.ArrayList;
+import it.polimi.ingsw.model.Dashboard;
 
 public class Player {
 
@@ -11,6 +12,7 @@ public class Player {
     private final Dashboard dashboard = new Dashboard();
     private int coins;
     private Assistant cardChosen;
+    private ArrayList<Color> professors;
 
     public Player(){
 
@@ -38,8 +40,9 @@ public class Player {
 
 
 
-    public Assistant drawCard() {
-
+    //we assume that the card is chosen through the passage of the index of the card inside the deck
+    public void drawCard(int cardIndex) throws EmptyDecktException{
+        cardChosen = deck.draw(cardIndex);
     }
 
     public void chooseCloud( ) {
@@ -47,18 +50,52 @@ public class Player {
     }
 
     public void addCoin() {
+        coins += coins;
+    }
+
+    public void removeCoin() throws LowerLimitException {
+        if (coins > 0)
+        {
+            coins -= coins;
+        }else{
+            throw new LowerLimitException();
+        }
+    }
+
+    public void changeState(State nextState) {
+            if(!(state.equals(nextState)))
+            {
+                state = nextState;
+            }
+    }
+
+    //methods of the class dashboard
+    public void addStudent (Student students) throws MaxSizeException {
+        dashboard.addStudent(students);
+    }
+
+    public void takeStudent (Color c) throws noStudentException {
+        dashboard.takeStudent(c);
+
+    }
+    public void putTower () throws fullTowersException {
+        dashboard.putTower();
 
     }
 
-    public void removeCoin() {
-
-    }
-
-    public void changeState() {
-
+    public void getTower () throws noTowersException {
+        dashboard.getTower();
     }
 
     public boolean hasProfessor(Color color){
         return false;
     }
+
+    public class  LowerLimitException extends Exception {
+        LowerLimitException(){super("you have reached the minimum number of coins");}
+
+    }
+
+
+
 }
