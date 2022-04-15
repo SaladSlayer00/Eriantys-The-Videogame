3fa9fffd292503957b325cf85ac85d583a39ec9d
mcrin <alice.maccarini@mail.polimi.it>
2022-04-15 11:54:45 +0200

    prepsrazione delle carte personaggio per la modlaità esperto

--- src/main/java/it/polimi/ingsw/model/Character.java
+++ src/main/java/it/polimi/ingsw/model/Character.java
@@ -1,6 +1,6 @@
 package it.polimi.ingsw.model;
 //class representing the character cards of the expert game mode
-public class Character{
+public abstract class Character{
     //attributes of the class Character
     private int cost;
     private int numCoins;
