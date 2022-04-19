ae17c9726e30b1d52ef862ac05fa0e52493d1ab3
mcrin <alice.maccarini@mail.polimi.it>
2022-04-19 10:09:51 +0200

    okay, questa roba è più complicata di quanto mi aspettassi...

---
+++ src/test/java/it/polimi/ingsw/model/test/PlayerTest.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw.model.test;
+
+import it.polimi.ingsw.model.Player;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+//this should be the class that allows to test the class Player
+public class PlayerTest{
+
+    Player playert;
+    private static final int playerID = 0;
+    private static final String name = "ptest";
+
+}
