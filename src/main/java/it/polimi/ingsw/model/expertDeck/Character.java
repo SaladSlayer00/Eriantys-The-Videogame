7c64f37dc99f9a15c326504940b0c3cbf3b9faa5
mcrin <alice.maccarini@mail.polimi.it>
2022-05-05 11:01:45 +0200

    RAGA LE CARTE SPERTO DEL CAZZO MI STANNO FACENDO IMPAZZIRE

--- src/main/java/it/polimi/ingsw/model/expertDeck/Character.java
+++ src/main/java/it/polimi/ingsw/model/expertDeck/Character.java
@@ -2,6 +2,13 @@ package it.polimi.ingsw.model.expertDeck;
 
 import it.polimi.ingsw.model.Player;
 
+/* TODO
+* GUYS. THE FACADE PATTER WON'T WORK HERE (or at least i think...)
+* the idea might be to use an adapter or maybe a wrapper???
+* we should probably discuss this thing more deeply but i think the adapter pattern might actually work
+* i hope so at least, because in other cases the only thing that might work that comes to my mind is an enormous case switch
+ */
+
 //class representing the character cards of the expert game mode
 public abstract class Character{
     //attributes of the class Character
