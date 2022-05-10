6778d8753b5d9cb507b13b91abac607fdcb82741
mcrin <alice.maccarini@mail.polimi.it>
2022-05-10 11:48:09 +0200

    boh onestamente spero sia giusto... ho toccato la virtual view cmq

--- src/main/java/it/polimi/ingsw/view/VirtualView.java
+++ src/main/java/it/polimi/ingsw/view/VirtualView.java
@@ -45,50 +45,56 @@ public class VirtualView implements View{
         clientHandler.sendMessage(new MoveMessage(GameFactory.SERVER_NICKNAME, MessageType.MOVE, availableStudents));
     }
 
+    //guys i think we should have TWO METHODS for moving the students around
+    //one for moving the students on the islands and the other one to move the students on the proper row
+    //TODO
     @Override
     public void askMoves(List<Position> availablePosition) {
-
+        clientHandler.sendMessage(new MoveMessage(GameFactory.SERVER_NICKNAME, MessageType.MOVE_ON_BOARD, availablePosition));
     }
 
     @Override
     public void askMotherMoves(int possibleSteps) {
-
+        clientHandler.sendMessage(new MoveMotherMessage(GameFactory.SERVER_NICKNAME, MessageType.MOVE_MOTHER, possibleSteps));
     }
 
     @Override
     public void askCloud(List<Cloud> availableClouds) {
-
+        clientHandler.sendMessage(new PickCloudMessage(GameFactory.SERVER_NICKNAME, MessageType.GET_FROM_CLOUD, availableClouds));
     }
 
+    //TODO
     @Override
     public void askPlayersNumber() {
-
+        clientHandler.sendMessage(new PlayerNumberReply(GameFactory.SERVER_NICKNAME, MessageType.PLAYERNUMBER_REPLY, ???));
     }
 
+    //TODO
     @Override
     public void askGameMode() {
-
+        clientHandler.sendMessage(new GameModeReply(GameFactory.SERVER_NICKNAME, MessageType.GAMEMODE_REPLY, ???));
     }
 
+    //TODO
     @Override
     public void askInitType(List<Type> teams) {
-
+        clientHandler.sendMessage()
     }
 
     @Override
-    //TODO
+    //TODO ??? is this thing right?????????
     public void showGenericMessage(String genericMessage) {
            System.out.println(genericMessage);
     }
 
+    //TODO
     @Override
     public void showLoginResult(boolean nicknameAccepted, boolean connectionResult, String nickname) {
-
     }
 
+    //TODO
     @Override
     public void showDisconnectionMessage(String playerDisconnected) {
-
     }
 
     @Override
