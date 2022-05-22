723a1b5a04bed950980d43bc7cdb4471430ab67c
mcrin <alice.maccarini@mail.polimi.it>
2022-05-22 15:21:29 +0200

    ho deciso di modificare la classe per la scelta della modalità di gioco

--- src/main/java/it/polimi/ingsw/view/gui/scenes/GameModeSceneController.java
+++ src/main/java/it/polimi/ingsw/view/gui/scenes/GameModeSceneController.java
@@ -14,44 +14,5 @@ import javafx.scene.input.MouseEvent;
  */
 public class GameModeSceneController extends ViewObservable implements BasicSceneController {
 
-    @FXML
-    private Button confirmButton;
 
-    @FXML
-    private Button backToMainButton;
-
-    @FXML
-    private RadioButton radioButtonROne;
-
-    @FXML
-    private RadioButton radioButtonRTwo;
-
-    @FXML
-    private ToggleGroup tG;
-
-    private String easyMode;
-    private String expertMode;
-
-    //basic constructor
-    public void GameModeSceneController(){
-        easyMode = null;
-        expertMode = null;
-    }
-
-    @FXML
-    public void initialize(){
-        radioButtonROne.setText(easyMode + " Mode");
-        radioButtonRTwo.setText(expertMode + "Mode");
-
-        confirmButton.addEventHandler(MouseEvent.MOUSE_CLICKED, this::onConfirmButtonClick);
-        backToMainButton.addEventHandler(MouseEvent.MOUSE_CLICKED, this::onBackToMainButtonClick);
-    }
-
-    //this is to handle the clicks on the confirm button
-    //the parameter is actually the event of the button clicked on by the mouse
-    private void onConfirmButtonClick(Event event){
-        confirmButton.setDisable(true);
-        RadioButton selectedButton = (RadioButton) tG.getSelectedToggle();
-
-    }
 }
