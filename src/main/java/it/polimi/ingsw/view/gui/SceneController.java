61c4de1104436d02dc900bc63e62f0afdc719d13
mcrin <alice.maccarini@mail.polimi.it>
2022-05-24 00:12:39 +0200

    scritto un po' di sceneController per la gui

--- src/main/java/it/polimi/ingsw/view/gui/SceneController.java
+++ src/main/java/it/polimi/ingsw/view/gui/SceneController.java
@@ -1,6 +1,81 @@
 package it.polimi.ingsw.view.gui;
 
+import it.polimi.ingsw.client.Client;
 import it.polimi.ingsw.observer.ViewObservable;
+import it.polimi.ingsw.observer.ViewObserver;
+import it.polimi.ingsw.view.gui.scenes.BasicSceneController;
+import javafx.event.Event;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import java.io.IOException;
+import java.util.List;
 
 public class SceneController extends ViewObservable {
+
+    //should there be a String for the prefix???
+
+    private static Scene activeScene;
+    public static BasicSceneController activeSceneController;
+
+    //getter for the active scene
+    public Scene getActiveScene(){
+        return activeScene;
+    }
+
+    //getter for the active scene controller
+    public BasicSceneController getSceneController(){
+        return activeSceneController;
+    }
+
+    //this is the method that changes the root panel
+    //it returns the new scene controller
+    public static <T> T changeRootPane(List<ViewObserver> listOfObserver, Scene actualScene, String fxml){
+        T controller = null;
+
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(SceneController.class.getResource("/fxml/" + fxml ));
+            System loader;
+            Parent root = fxmlLoader.load();
+            controller = fxmlLoader.getController();
+            ((ViewObservable) controller).addAllObservers(listOfObserver);
+            activeSceneController = (BasicSceneController) controller;
+            activeScene = actualScene;
+            activeScene.setRoot(root);
+        } catch(IOException exception){
+            Client.LOGGER.severe(exception.getMessage());
+        }
+        return controller;
+    }
+
+    //another one
+    public static <T> T changeRootPane(List<ViewObserver> listOfObserver, Event event, String fxml){
+        Scene actualScene = ((Node) event.getSource()).getScene();
+        return changeRootPane(listOfObserver, actualScene, fxml);
+    }
+
+    //another one
+    public static void changeRootPane(BasicSceneController bsController, Scene scene, String fxml){
+        try{
+            FXMLLoader loader = new FXMLLoader(SceneController.class.getResource("/fxml/" + fxml));
+            loader.setController(bsController);
+            activeSceneController = bsController;
+            Parent root = loader.load();
+            activeScene = scene;
+            activeScene.setRoot(root);
+        } catch(IOException exception){
+            Client.LOGGER.severe(exception.getMessage());
+        }
+    }
+
+    public static void changeRootPane(BasicSceneController bsController, String fxml){
+        changeRootPane(bsController, activeScene, fxml);
+    }
+
+    //this should show the client message in a popup
+    public static void alertShown(String mainTitle, String text){
+        //TODO (i'll keep coding tomorrow cuz I'm quite tired tbh)
+    }
 }
