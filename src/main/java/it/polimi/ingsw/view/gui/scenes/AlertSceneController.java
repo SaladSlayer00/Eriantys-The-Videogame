00ce4aa415604aef64276f81cd7b330c6e9315e4
mcrin <alice.maccarini@mail.polimi.it>
2022-05-24 12:52:04 +0200

    scritto un po' di sceneController per la gui

--- src/main/java/it/polimi/ingsw/view/gui/scenes/AlertSceneController.java
+++ src/main/java/it/polimi/ingsw/view/gui/scenes/AlertSceneController.java
@@ -1,5 +1,13 @@
 package it.polimi.ingsw.view.gui.scenes;
 
+import javafx.scene.Scene;
+
+//TODO
 public class AlertSceneController extends BasicSceneController {
 
+    //various methods
+
+    public void setScene(Scene scene){
+
+    }
 }
