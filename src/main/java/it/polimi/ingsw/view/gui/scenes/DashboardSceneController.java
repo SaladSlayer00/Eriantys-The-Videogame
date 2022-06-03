b2bb086952113a73caa2f74db0318c488dd3c32b
mcrin <alice.maccarini@mail.polimi.it>
2022-06-03 16:48:57 +0200

    continuato la gui

---
+++ src/main/java/it/polimi/ingsw/view/gui/scenes/DashboardSceneController.java
@@ -0,0 +1,15 @@
+package it.polimi.ingsw.view.gui.scenes;
+
+public class DashboardSceneController implements BasicSceneController {
+    //TODO
+    /* i don't really know how to handle the gamefield part yet tbh...
+    * IDEA
+    * - the gameboard is on the top of the screen and then each player has their own dashboard below
+    *   near the dashboard there is the mage image and then there is a popup menu for the expert card above the gameboard
+    *   game info on the top right of the screen and a chronometer of the centre
+    * - the gameboard is in the centre of the screen and then there is another scene just for the dashboard
+    *   each player can see all the dashboard at once so that is more easy to understand the game situation and make strategy
+    *   there still is a game info box on the top right of the screen, cuz i think it might be cute
+    * - ...?
+     */
+}
