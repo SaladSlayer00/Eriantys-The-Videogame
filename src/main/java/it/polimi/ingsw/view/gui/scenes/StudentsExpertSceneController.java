011c5dff6629ba69d9741f28ae7316801852d2b9
mcrin <alice.maccarini@mail.polimi.it>
2022-06-23 22:14:20 +0200

    kept working on the expert mode (gui verse)

--- src/main/java/it/polimi/ingsw/view/gui/scenes/StudentsExpertSceneController.java
+++
@@ -1,8 +0,0 @@
-package it.polimi.ingsw.view.gui.scenes;
-
-import it.polimi.ingsw.observer.ViewObservable;
-
-public class StudentsExpertSceneController extends ViewObservable implements BasicSceneController{
-
-
-}
