0458460893b5562d7f0528e7e9d474b2d825555f
ThetaKimbi <101363816+ThetaKimbi@users.noreply.github.com>
2022-06-30 23:25:19 +0200

    Bug fixing

--- src/main/java/it/polimi/ingsw/ClientApp.java
+++ src/main/java/it/polimi/ingsw/ClientApp.java
@@ -12,14 +12,13 @@ import static java.lang.System.out;
 
 // Java program to demonstrate working of Scanner in Java
 import java.util.Scanner;
-
+/**
+ * Main of the client app.
+ */
 public class ClientApp {
     public static void main(String[] args) {
 
         boolean cliParam = false; // default value
-        out.print("Scegliere CLI");
-        Scanner in = new Scanner(System.in);
-        String answer = in.nextLine();
         for (String arg : args) {
             if (arg.equals("--cli") || arg.equals("-c")) {
                 cliParam = true;
@@ -27,14 +26,14 @@ public class ClientApp {
             }
         }
         
-        if (answer.equals("cli")) {
+        if (cliParam) {
             Client.LOGGER.setLevel(Level.WARNING);
             Cli view = new Cli();
             ClientController clientcontroller = new ClientController(view);
             view.addObserver(clientcontroller);
             view.init();
         } else {
-            //Application.launch(GuiJavaFX.class);
+            Application.launch(GuiJavaFX.class);
         }
     }
 }
